4f0e706282cc08629d299144437096c095ce1709
octaviocarpes <octavio.carpes10@gmail.com>
2021-03-11 20:29:48 -0300

    Finished poc

--- client/src/layouts/HomePage/index.tsx
+++ client/src/layouts/HomePage/index.tsx
@@ -19,7 +19,9 @@ import {
 	FormErrorMessage,
 	useToast,
 	FormControl,
-	Link
+	Link,
+	Divider,
+	useBreakpointValue,
 } from '@chakra-ui/react'
 import { Icon, SearchIcon } from '@chakra-ui/icons'
 import { User } from '../../models/User'
@@ -27,6 +29,7 @@ import { useForm } from 'react-hook-form'
 import { GET_USER } from '../../graphql/queries/getUser'
 import { client } from '../../graphql/client'
 import { BsFolderFill } from 'react-icons/bs'
+import { AiOutlineFork, AiFillStar } from 'react-icons/ai'
 
 
 interface PropTypes {
@@ -35,6 +38,17 @@ interface PropTypes {
 
 const HomeLayout: React.FC<PropTypes> = memo((props: PropsWithChildren<any>): ReactElement => {
 	const toast = useToast()
+	const breakpoint = useBreakpointValue<'row' | 'column'>({
+		base: 'column',
+		xs: 'column',
+		sm: 'row',
+		md: 'row',
+		lg: 'row',
+		xl: 'row'
+	})
+
+	console.log(breakpoint)
+
 	const [user, setUser] = useState<User>({
 		id: 0,
 		name: '',
@@ -48,8 +62,14 @@ const HomeLayout: React.FC<PropTypes> = memo((props: PropsWithChildren<any>): Re
 	const [isLoading, setIsLoading] = useState(false)
 	const { register, errors, handleSubmit } = useForm()
 
-
-	const paddingSize = '16%'
+	const boxShadow = `
+  		0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+  		0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+  		0 12.5px 10px rgba(0, 0, 0, 0.035),
+  		0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+  		0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+  		0 100px 80px rgba(0, 0, 0, 0.07);
+	`
 
 	const onSubmit = async ({ username }: { username: string }) => {
 		setIsLoading(true)
@@ -78,6 +98,7 @@ const HomeLayout: React.FC<PropTypes> = memo((props: PropsWithChildren<any>): Re
 					>
 						<Flex
 							alignItems={'center'}
+							flexDirection={breakpoint}
 						>
 							<Box>
 								<Avatar
@@ -104,15 +125,30 @@ const HomeLayout: React.FC<PropTypes> = memo((props: PropsWithChildren<any>): Re
 						</Heading>
 						<List>
 							{user.repos.map(repository => (
-								<ListItem marginBottom={4} key={repository.id}>
-									<Box  border={'1px solid'} borderRadius={'5px'} padding={2}>
+								<ListItem   border={'1px solid lightgray'} borderRadius={'5px'} marginBottom={4} key={repository.id}>
+									<Box padding={2}>
 										<Flex alignItems={'center'} marginBottom={2}>
-											<Icon as={BsFolderFill} marginRight={2} />
-											<Link isExternal href={repository.html_url}>
-												<Heading size={'sm'}>{repository.full_name}</Heading>
+											<Icon color={'blue.500'} as={BsFolderFill} marginRight={2} />
+											<Link color={'blue.500'} isExternal href={repository.html_url}>
+												<Heading fontSize={['xs', 'sm', 'md', 'lg']}>{repository.full_name}</Heading>
 											</Link>
 										</Flex>
-										<Text>{repository.description}</Text>
+										<Text marginBottom={4}>{repository.description}</Text>
+										<Flex>
+											<Box marginRight={8}>
+												<Flex alignItems={'center'}>
+													<Icon as={AiFillStar} marginRight={2} />
+													<Text>Stars {repository.stargazers_count}</Text>
+												</Flex>
+											</Box>
+
+											<Box>
+												<Flex alignItems={'center'}>
+													<Icon as={AiOutlineFork} marginRight={2} />
+													<Text>Forks {repository.forks}</Text>
+												</Flex>
+											</Box>
+										</Flex>
 									</Box>
 								</ListItem>
 							))}
@@ -132,65 +168,74 @@ const HomeLayout: React.FC<PropTypes> = memo((props: PropsWithChildren<any>): Re
 			paddingLeft={['2%', '4%', '10%', '15%']}
 			paddingTop={'4%'}
 		>
-			<Flex
-				justifyContent={'center'}
-				alignItems={'center'}
+			<Box
+				boxShadow={boxShadow}
+				backgroundColor={'white'}
+				padding={8}
+				borderRadius={'5px'}
 			>
-				<Box>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<Flex>
-							<FormControl isInvalid={errors.username}>
-								<InputGroup
-									marginBottom={4}
-								>
-									<InputLeftElement pointerEvents={'none'} marginRight={6}>
-										<SearchIcon color={'gray.300'} />
-									</InputLeftElement>
-									<Input
-										width={['200px', '300px', '400px', '500px']}
-										height={['36px', '36px', '40px', '40px']}
-										type={'text'}
-										name={'username'}
-										id={'username'}
-										placeholder={'Search for a user on Github'}
-										errorBorderColor={'crimson'}
-										isInvalid={errors.username}
-										ref={register({ required: true })}
-									/>
-								</InputGroup>
-								<FormErrorMessage>Username is required</FormErrorMessage>
-							</FormControl>
-							<Button
-								isLoading={isLoading}
-								type={'submit'}
-							>
-								Search
-							</Button>
-						</Flex>
-					</form>
-				</Box>
-			</Flex>
-			<Grid
-				width={'100%'}
-			>
-				{ isLoading ? (
+				<Flex
+					justifyContent={'center'}
+					alignItems={'center'}
+				>
 					<Box>
-						<Flex marginBottom={8}>
-							<SkeletonCircle size={'20'} marginRight={4} />
-							<Box width={'100%'}>
-								<SkeletonText mt="4" noOfLines={4} spacing="4" />
-							</Box>
-						</Flex>
-						<Box>
-							<Stack>
-								<Skeleton height="20px" />
-								<Skeleton height="20px" />
-								<Skeleton height="20px" />
-							</Stack>
-						</Box>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<Flex>
+								<FormControl isInvalid={errors.username}>
+									<InputGroup
+										marginBottom={4}
+									>
+										<InputLeftElement pointerEvents={'none'} marginRight={6}>
+											<SearchIcon color={'gray.300'} />
+										</InputLeftElement>
+										<Input
+											width={['200px', '300px', '400px', '500px']}
+											height={['36px', '36px', '40px', '40px']}
+											type={'text'}
+											name={'username'}
+											id={'username'}
+											placeholder={'Search for a user on Github'}
+											errorBorderColor={'crimson'}
+											isInvalid={errors.username}
+											ref={register({ required: true })}
+										/>
+									</InputGroup>
+									<FormErrorMessage>Username is required</FormErrorMessage>
+								</FormControl>
+								<Button
+									isLoading={isLoading}
+									type={'submit'}
+								>
+									Search
+								</Button>
+							</Flex>
+						</form>
 					</Box>
-				): renderUserData()}
-			</Grid>
+				</Flex>
+				<Divider marginTop={6} marginBottom={6} />
+				<Grid
+					width={'100%'}
+				>
+					{ isLoading ? (
+						<Box>
+							<Flex marginBottom={8}>
+								<SkeletonCircle size={'20'} marginRight={4} />
+								<Box width={'100%'}>
+									<SkeletonText mt="4" noOfLines={4} spacing="4" />
+								</Box>
+							</Flex>
+							<Box>
+								<Stack>
+									<Skeleton height="20px" />
+									<Skeleton height="20px" />
+									<Skeleton height="20px" />
+								</Stack>
+							</Box>
+						</Box>
+					): renderUserData()}
+				</Grid>
+			</Box>
+			<Box height={'24px'} />
 		</Box>
 	)
 })
